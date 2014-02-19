325c342a183bcdf531a78cf6c1b2850cc6dc3db9
Shaun Williams <shaunewilliams@gmail.com>
2014-02-19 14:43:42 -0600

    changed problem 1 solution to use naming standards

--- src/cljs/euler_cljs/problem1.cljs
+++ src/cljs/euler_cljs/problem1.cljs
@@ -1,8 +1,9 @@
 (ns euler.problem1)
 
-(defn multOfAny [n factors] (some #(zero? (mod n %)) factors))
+(defn divisible-by-any? [n factors]
+  (some #(zero? (mod n %)) factors))
 
 (defn answer []
   (->> (range 1 1000)
-       (filter #(multOfAny % [3 5]))
+       (filter #(divisible-by-any? % [3 5]))
        (reduce +)))
