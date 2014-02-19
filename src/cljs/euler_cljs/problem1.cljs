66c39b1bcc6a5a1a77570af06138277713d4e009
Shaun Williams <shaunewilliams@gmail.com>
2014-02-19 11:40:13 -0600

    added solution to problem 1

--- src/cljs/euler_cljs/problem1.cljs
+++ src/cljs/euler_cljs/problem1.cljs
@@ -1,4 +1,8 @@
 (ns euler)
 
-(defn problem1 []
-    "hello")
+(defn multOf3or5 [n]
+    (or
+        (= 0 (mod n 3))
+        (= 0 (mod n 5))))
+
+(defn problem1 [] (reduce + (filter multOf3or5 (range 1 1000))))
