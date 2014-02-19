882279ab2d8504b7d9e610d1d3afd1aae5ff4382
Shaun Williams <shaunewilliams@gmail.com>
2014-02-19 13:49:45 -0600

    added standard indent style and used 'thread-last' macro to simplify form nesting

--- src/cljs/euler_cljs/problem1.cljs
+++ src/cljs/euler_cljs/problem1.cljs
@@ -1,8 +1,11 @@
 (ns euler.problem1)
 
 (defn multOf3or5 [n]
-    (or
-        (= 0 (mod n 3))
-        (= 0 (mod n 5))))
+  (or
+    (= 0 (mod n 3))
+    (= 0 (mod n 5))))
 
-(defn answer [] (reduce + (filter multOf3or5 (range 1 1000))))
+(defn answer []
+  (->> (range 1 1000)
+       (filter multOf3or5)
+       (reduce +)))
--- src/cljs/euler_cljs/problem2.cljs
+++ src/cljs/euler_cljs/problem2.cljs
@@ -3,4 +3,8 @@
 (defn fib [[a b]] [b (+ a b)])
 (defn fib-seq [] (map first (iterate fib [1 2])))
 
-(defn answer [] (reduce + (filter even? (take-while #(<= % 4000000) (fib-seq)))))
+(defn answer []
+  (->> (fib-seq)
+       (take-while (partial >= 4000000))
+       (filter even?)
+       (reduce +)))
