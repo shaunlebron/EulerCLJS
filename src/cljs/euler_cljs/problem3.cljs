93f405e2eb9ad2e3a4449adae24a24b328e248fb
Shaun Williams <shaunewilliams@gmail.com>
2014-02-21 16:01:50 -0600

    added slow but interesting solution to problem 3 using lazy sequences

--- src/cljs/euler_cljs/problem3.cljs
+++ src/cljs/euler_cljs/problem3.cljs
@@ -4,10 +4,21 @@
 (def sqrt (.-sqrt js/Math))
 (def floor (.-floor js/Math))
 
-; I struggled with creating a prime-sieve function.  I first implemented one using sets, but it never finished executing.
-; I took this solution from 'cjnirehs' at http://clojure-euler.wikispaces.com/Problem+003 which taught me how to
-; use filters to create virtual sets (i.e. lazily-evaluated sets based on filtered conditions).  Wow!
-; TODO: implement solution
+; I struggled with creating a prime-sieve function.  I first implemented one using sets, but it never finished executing...
+; Instead, I took this solution from 'cjnirehs' at http://clojure-euler.wikispaces.com/Problem+003 which taught me how to
+; use filters to create virtual sets (i.e. lazily-evaluated sets based on condition filters).  Wow!
+; (Actually, after implementing, it's still really slow and never finishes in the browser; still interesting solution though)
+(defn get-primes [n]
+  (loop [p 2 sieve (range 2 n)]
+    (if (> (* p p) n)
+      sieve
+      (let [next-sieve (filter #(or (= % p) (not= 0 (mod % p))) sieve)
+            next-p (first (filter #(> % p) next-sieve))]
+        (recur next-p next-sieve)))))
+
+(defn max-prime-factor-sieve [number]
+  (let [primes (get-primes number)]
+    (last (filter #(zero? (mod number %)) primes))))
 
 ; This solution is even better, by 'achim_p'.  It uses simple tail loop to linearly search and divide out factors from the number.
 (defn max-prime-factor [number]
