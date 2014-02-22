00b0c53214dc9b3965d38df7f6ef0a48ab298552
Shaun Williams <shaunewilliams@gmail.com>
2014-02-22 11:39:50 -0600

    tried to add intuitive explanation for the easiest way to compute GCD

--- src/cljs/euler_cljs/problem5.cljs
+++ src/cljs/euler_cljs/problem5.cljs
@@ -1,5 +1,26 @@
 (ns euler.problem5)
 
+; I was able to remember the simple formula for computing the GCD of two numbers A and B
+; becuase I had sought an intuitive explanation for why it works:
+;
+; The euclidean algorithm hides something really beautiful and intuitive about "units".
+
+;                 A = X units
+; |-------------------------------------|
+; |-----------------|
+;    B = Y units    |-------------------|
+;                    (A-B) = (X-Y) units
+;
+;              The max possible "unit" size we can use to represent both A and B
+;              must be the same for (A-B).  You might have to stare above to realize this.
+;             
+;              So max_unit(A,B) = max_unit(B, A-B)
+;
+;                 (Which also means that it's equal to max_unit(B, A mod B) by extension.)
+;
+;              "max_unit" = "greatest common factor"  (they mean the same thing)
+
+; 
 (defn gcd [a b]
   (cond
     (< a b) (recur b a)
